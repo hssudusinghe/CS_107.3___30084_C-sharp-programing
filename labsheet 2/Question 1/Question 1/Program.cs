b824650889e60c02f6ea616c115e31c666add637
hssudusinghe <135609482+hssudusinghe@users.noreply.github.com>
2024-02-20 23:37:17 -0800

    Add files via upload

---
+++ labsheet 2/Question 1/Question 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace lab_2
+    {
+        using System;
+
+        class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Instantiate an object of the Book class
+                Book myBook = new Book();
+
+                // Set values for the properties
+                myBook.Title = "Madol Duwa";
+                myBook.Author = "Martin Wikramasingha";
+
+                // Display the information on the console
+                Console.WriteLine("Book Title: " + myBook.Title);
+                Console.WriteLine("Author: " + myBook.Author);
+
+                Console.ReadLine(); // Keep the console window open
+            }
+        }
+
+    }   }   
---
+++ labsheet 2/Question 2/Question 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    
+        using System;
+
+        class BankAccount
+        {s
+            public string AccountNumber { get; set; }
+            public double Balance { get; private set; }
+
+            public BankAccount(string accountNumber, double initialBalance)
+            {
+                AccountNumber = accountNumber;
+                Balance = initialBalance;
+            }
+
+            public void Deposit(double amount)
+            {
+                if (amount <= 0)
+                {
+                    Console.WriteLine("Deposit amount must be greater than zero.");
+                }
+                else
+                {
+                    Balance += amount;
+                    Console.WriteLine("Deposit of $" + amount + " successful.");
+                }
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Instantiate a BankAccount object
+                BankAccount myAccount = new BankAccount("123456789", 100.0);
+
+                // Display initial balance
+                Console.WriteLine("Initial Balance: $" + myAccount.Balance);
+
+                // Deposit money into the account
+                double depositAmount = 50.0;
+                myAccount.Deposit(depositAmount);
+
+                // Display updated balance
+                Console.WriteLine("Updated Balance: $" + myAccount.Balance);
+
+                Console.ReadLine(); // Keep the console window open
+            }
+        }
+
+
+    }
+   
